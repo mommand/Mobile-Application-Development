943019b036caceedeeeff76abb4dfc19af71888e
mommand <zmomand@technation.af>
2018-06-10 00:39:08 +0430

    last upload

---
+++ Fregment/src/com/demo/fregment/Fregment2.java
@@ -0,0 +1,18 @@
+package com.demo.fregment;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fregment2 extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,  
+            Bundle savedInstanceState) {  
+        // TODO Auto-generated method stub  
+        return inflater.inflate(R.layout.fregment2,container, false);  
+    }  
+
+
+}
